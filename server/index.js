d4c9c69df74425c742739e0fe7cc71f383484493
Saurabh2Devloper <saurabh.mhatre711@gmail.com>
2024-01-08 19:50:01 +0530

    DOne

---
+++ server/index.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 5000; // Or any port you prefer
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// MongoDB connection (assuming local)
+mongoose.connect('mongodb+srv://saurabhmhatre:saurabhmhatre@cluster0.cmxgmkz.mongodb.net/todos?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const todoSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+});
+
+const Todo = mongoose.model('Todo', todoSchema);
+
+// Routes
+app.get('/todos', async (req, res) => {
+  try {
+    const todos = await Todo.find();
+    res.json(todos);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+app.post('/todos', async (req, res) => {
+  const todo = new Todo({
+    title: req.body.title,
+    description: req.body.description,
+  });
+  try {
+    const newTodo = await todo.save();
+    res.status(201).json(newTodo);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+app.delete('/todos/:id', async (req, res) => {
+  try {
+    await Todo.deleteOne({ _id: req.params.id });
+    res.json({ message: 'Todo deleted' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+app.put('/todos/:id', async (req, res) => {
+  try {
+    const todo = await Todo.findById(req.params.id);
+    todo.title = req.body.title;
+    todo.description = req.body.description;
+    const updatedTodo = await todo.save();
+    res.json(updatedTodo);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
